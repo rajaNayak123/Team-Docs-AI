68b4cea7f7749ce9ed26b5c343f72d08e0565df5
rajaNayak123 <nayakraja151@gmail.com>
2025-07-31 12:37:19 +0530

    update the decler variables

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,26 +1,32 @@
+// next-auth.d.ts
+
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
+
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      name: string
-      email: string
-      teamId: string
-      role: string
-      isPro: boolean
-    }
+      id: string;
+      teamId?: string;
+      role?: string;
+      isPro?: boolean;
+    } & DefaultSession["user"]; // Keep the default properties
   }
 
-  interface User {
-    teamId: string
-    role: string
-    isPro: boolean
+  interface User extends DefaultUser {
+    // Add your custom properties to the User object
+    teamId?: string;
+    role?: string;
+    isPro?: boolean;
   }
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
-    teamId: string
-    role: string
-    isPro: boolean
+  interface JWT extends DefaultJWT {
+    // Add your custom properties to the JWT
+    id: string;
+    teamId?: string;
+    role?: string;
+    isPro?: boolean;
   }
 }
