fa25439a199e38f6b22aac75e491732e0b466d57
rajaNayak123 <nayakraja151@gmail.com>
2025-07-31 12:13:30 +0530

    declar the global variable

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,16 @@
-declare global {
+// declare global {
+//     var mongoose: {
+//       conn: any
+//       promise: any
+//     }
+//   }
+  
+//   export {}
+  
+  declare global {
     var mongoose: {
-      conn: any
-      promise: any
+      conn: typeof import("mongoose") | null
+      promise: Promise<typeof import("mongoose")> | null
     }
   }
   
